1b2f6468d6ef6c3db87c845caef57a8c9634ea91
lawty <kevin.delafontaine77@gmail.com>
2012-10-11 14:26:49 +0200

    Question 2 projet + test commit

---
+++ TPGetapTU/src/org/ldv/sio/getap/Exemple.java
@@ -0,0 +1,15 @@
+package org.ldv.sio.getap;
+
+import java.sql.Date;
+
+
+
+public class Exemple {
+	
+	public static void main(String arg[]){
+		
+		/* Creation d'une boucle for pour la question 3 a mediter */	
+	}
+	
+}
+
